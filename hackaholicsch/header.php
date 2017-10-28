60811735fc99d196f407143dbbd32ec729b1b4d8
Maneesha Rajaratne <maneesharajaratne@gmail.com>
2017-10-28 14:30:57 +0530

    Add files via upload

---
+++ hackaholicsch/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/header.css">
+      <link rel="stylesheet" href="css/dashboard.css">
+      <link rel="stylesheet" href="css/types.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+
+
+  </head>
+  <body>
+
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="login/register.php">Register</a>
+      <a href="login/login.php">Login</a>
+    </div>
+
+    <div class="container">
+      <div class="topnav" id="topNav">
+        <a href="#" onclick="openNav()"><i class="material-icons">account_circle</i></a>
+        <a href="#"></a>
+        <a href="dashboard.php"><i class="material-icons" id="dashboard-icon">home</i></a>
+        <a href="types.php">Types sexual harrasements</a>
+        <a href="prevention.html"target="">Prevention</a>
+        <a href="situation.html"target="main">How to face a situation</a>
+        <a href="contact.php"target="main">Contact phsycologist</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+
+      </div>
+
+      <script src="js/header.js"></script>
+    </div>
+
